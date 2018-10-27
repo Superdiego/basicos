c61b8060aba18d30f62919b55dac01072590ffa0
DCP <DCP@10.0.2.15>
2018-10-27 14:39:12 +0200

    Añadido Lógicos

--- src/basicos_diegocandela/Basicos_DiegoCandela.java
+++ src/basicos_diegocandela/Basicos_DiegoCandela.java
@@ -16,6 +16,23 @@ public class Basicos_DiegoCandela {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    logicos();
     }
-    
+   /**
+     * Método para mostrar ejemplos de tipos lógioos.
+     */
+    public static void logicos() {
+
+        //Estos son los tipos básicos
+        boolean cotilleo;
+        
+
+        //Ejemplos de asignación de valores a las variables de tipos básicos
+        cotilleo = true;
+        
+
+        //Mostrar en pantalla el contendido de las variables de tipos básicos.
+        if(cotilleo==true)System.out.println("Ese rumor es cierto");
+        else System.out.println("");
+    } 
 }
