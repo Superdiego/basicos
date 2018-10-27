7fb0853dc1ae4091cdd717c955650bbfc04f99ee
DCP <DCP@10.0.2.15>
2018-10-27 10:21:20 +0200

    Creado numericos

--- src/basicos_diegocandela/Basicos_DiegoCandela.java
+++ src/basicos_diegocandela/Basicos_DiegoCandela.java
@@ -16,6 +16,49 @@ public class Basicos_DiegoCandela {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+         numericos();  // Llamada al método
+         
+                  
     }
-    
+         /**
+     * Método para mostrar ejemplos de tipos numericos.
+     */
+    public static void numericos() {
+
+
+       
+
+        //Ejemplos de asignación de valores a las variables de tipos básicos
+
+       //Estos son los tipos básicos
+        byte elMasPeque;
+        short chiquitin;
+        int entero;
+        long duplicaEntero;
+        float grande;
+        double doble;
+      
+        
+        entero = 10;
+        doble = 5.6;
+        elMasPeque= 124;
+        chiquitin= 32600;
+
+        
+        
+        
+        
+
+        //Mostrar en pantalla el contendido de las variables de tipos básicos.
+        System.out.print("Valor entero:  ");
+        System.out.println(entero);
+        System.out.print("Valor doble:  ");
+        System.out.println(doble);
+        System.out.print("Valor más pequeño:  ");
+        System.out.println(elMasPeque);
+        System.out.print("Valor pequeño:  ");
+        System.out.println(chiquitin);
+
+        
+    }
 }
