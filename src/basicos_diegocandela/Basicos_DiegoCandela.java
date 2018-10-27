7d30ea8b6726f4c6dca679667a20328ff59c472f
DCP <DCP@10.0.2.15>
2018-10-27 14:12:01 +0200

    Creado carácter

--- src/basicos_diegocandela/Basicos_DiegoCandela.java
+++ src/basicos_diegocandela/Basicos_DiegoCandela.java
@@ -16,6 +16,23 @@ public class Basicos_DiegoCandela {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        cadena();
     }
-    
+   /**
+     * Método para mostrar ejemplos de tipos carácter
+     */
+    public static void cadena() {
+
+        //Estos son los tipos básicos
+        String texto;
+       
+
+        //Ejemplos de asignación de valores a las variables de tipos básicos
+        texto = "Puedes escribir el texto que quieras";
+        
+
+        //Mostrar en pantalla el contendido de las variables de tipos básicos.
+        System.out.println(texto);
+
+    } 
 }
